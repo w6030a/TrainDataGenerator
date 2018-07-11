73a286ff391cd917e07545b83af43c64a582c831
Peter Chen <w6030bchen@gmail.com>
2018-07-10 23:25:31 -0700

    1.Add ideas to record.py 2.Add skeleton to train_data_generator.py

--- traindata/record.py
+++ traindata/record.py
@@ -1,10 +1,49 @@
 class Record:
     
+    # P.S. all amounts normalized by dividing by init total chips
+    
+    ## table info
+    # . round stage encoding [preflop, flop, river...]
+    # . total bet on table
+    
+    ## record owner info
+    # . current bet
+    # . current chips in hand
+    # . remaining chips (hand + reloads)
+    # . action encoding [call, bet, raise, allin, fold]
+    # . action amount
+    
+    ## opponent info
+    # . richest unfolded opponent's chips in hand
+    # . richest unfolded opponent's remaining chips (hand + reloads)
+    # . poorest unfolded opponent's chips in hand
+    # . poorest unfolded opponent's remaining chips (hand + reloads)
+    # . avg. of unfolded opponents' chips in hand
+    # . avg. of unfolded opponents' remaining chips (hand + reloads)
+    # . richest unfolded most aggressive(most raise) opponent's chips in hand
+    # . richest unfolded most defensive(most call) opponent's remaining chips (hand + reloads)
+    # . poorest unfolded most aggressive(most raise) opponent's chips in hand
+    # . poorest unfolded most defensive(most call) opponent's remaining chips (hand + reloads)
+    
+    # . # of unfolded opponents
+    # . # of survive opponents
+    # . # of all-in opponents
+    # . # of raises / 
+    # . # of bets
+    # . # of calls
+    
+    ## game info
+    # . game stage encoding; # of rounds / # of initial players [early if < 1, mid if < 2, late if else]
+    
     def __init__(self, table_id, stage, total_bet, init_chips, max_reload):
         # table info
         self.table_id = table_id
         self.stage = stage
+        
+        #TODO: normalize
         self.totla_bet = total_bet
+        
+        #TODO: may want to combine these to total initChips for Chips bet % feautre
         self.init_chips = init_chips
         self.max_reload = max_reload
         
--- traindata/train_data_generator.py
+++ traindata/train_data_generator.py
@@ -1,14 +1,68 @@
 import sys
+import json
+from record import Record
+from pprint import pprint
 
 def main(argv):
-    print  sys.argv[1:]
-    
+    print 'argv = {}'.format(sys.argv[1:])
+    processLog('C:\Users\pc\Desktop\pokerLog\common.log.game1_event.log')
     
 def readFile(file_path):
     f = open(file_path, 'r') 
     data =  f.read()
     f.close()
     return data
- 
+
+def processLog(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+        
+        for line in lines:
+            line = line[line.index('{'):]
+            json_obj = json.loads(line)
+            
+            if 'eventName' not in json_obj:
+                raise Exception('Not an event log')
+            
+            event_name = json_obj['eventName']
+            
+            if event_name == '__show_action':
+                record = process_show_action_event(json_obj)
+            elif event_name == '__round_end':
+                record = process_round_end_event(json_obj)
+            elif event_name == '__game_over':
+                record = process_game_over_event(json_obj)
+            else:
+                print json_obj
+                raise Exception('unrecognized event')
+        
+def process_show_action_event(json_obj):
+    record = Record()
+
+    data = json_obj['data']
+    player_info = data['players']
+    table_info = data['table']
+    action_info = data['action']
+
+    table_id = table_info['tableNumber']
+    stage = table_info['roundName']
+    total_bet = table_info['totalBet']
+    init_chips = table_info['initChips']
+    max_reload = table_info['max_reload']
+    
+    board_card = table_info['board']
+    #pprint(board_card)
+
+def process_round_end_event(json_obj):
+    data = json_obj['data']
+    player_info = data['players']
+    table_info = data['table']
+            
+    board_card = table_info['board']
+    #pprint(board_card)
+
+def process_game_over_event(json_obj):
+    print '__game_over event'
+
 if __name__ == "__main__":
     main(sys.argv)
